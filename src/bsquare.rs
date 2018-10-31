29015e3eeb10b6b532864f2119b29f5291bb26c9
Bart Massey <bart@cs.pdx.edu>
2018-10-31 15:16:16 -0700

    completed bsquare; cleaned up some stuff

--- src/bsquare.rs
+++ src/bsquare.rs
@@ -1,47 +1,54 @@
 extern crate portaudio;
 
 use portaudio as pa;
-use std::f64::consts::PI;
 
-const SAMPLE_RATE: u32 = 44_100;
-const FREQ: u32 = 1000;
-const MSECS: u32 = 3000;
-const FRAME_SIZE: u32 = 128;
-const NTABLE: usize = (SAMPLE_RATE * FREQ) as usize;
+const SAMPLE_RATE: i32 = 44_100;
+const FREQ: usize = 400;
+const MSECS: i32 = 3000;
+
+const FRAME_SIZE: u32 = 1024;
+const SAMPLES: usize = SAMPLE_RATE as usize * MSECS as usize / 1000;
+const FRAMES: usize =  SAMPLES / FRAME_SIZE as usize;
+const SAMPLES_PER_HALFCYCLE: i32 = (SAMPLE_RATE / (2 * FREQ as i32)) as i32;
 
 fn main() -> Result<(), pa::Error> {
+    println!("blocking square wave");
+    println!("sample_rate: {}, msecs: {}, freq: {}",
+            SAMPLE_RATE, MSECS, FREQ);
+    println!("frame_size: {}, frames: {}, halfcycle: {}",
+            FRAME_SIZE, FRAMES, SAMPLES_PER_HALFCYCLE);
+    println!("last frame nominal size: {}",
+             FRAME_SIZE as i32 * (FRAMES as i32 + 1) - SAMPLES as i32);
+
     let pa = pa::PortAudio::new()?;
     let settings =
         pa.default_output_stream_settings(1, SAMPLE_RATE as f64, FRAME_SIZE)?;
 
-
-    let scale =  2.0 * PI as f32 * FREQ as f32 / SAMPLE_RATE as f32;
-    println!("ntable: {}", NTABLE);
-    let mut table = vec![0.0; NTABLE];
-    for i in 0..NTABLE {
-        table[i] = (i as f32 * scale).sin();
-    }
-
-    let nsamples = MSECS * SAMPLE_RATE;
     let mut stream = pa.open_blocking_stream(settings)?;
     stream.start()?;
     
-    let mut sample = 0;
     let mut written = 0;
-    while written < nsamples {
-        let to_write = u32::min(nsamples - written, 1024);
-        println!("sample: {}, written: {}, to_write: {}", sample, written, to_write);
-        stream.write(to_write, |buffer| {
-            println!("writing");
-            for j in 0..(to_write as usize) {
-                buffer[j] = table[sample];
-                sample += 1;
-                if sample >= NTABLE {
-                    sample = 0;
+    let mut cycle = 0;
+    let mut sign = 0.8;
+    while written < SAMPLES as u32 {
+        let status = stream.write(FRAME_SIZE, |buffer| {
+            assert_eq!(buffer.len(), FRAME_SIZE as usize);
+            for i in 0..buffer.len() {
+                buffer[i] = sign;
+                cycle += 1;
+                if cycle >= SAMPLES_PER_HALFCYCLE {
+                    sign = -sign;
+                    cycle = 0;
                 }
             }
-        })?;
-        written += to_write;
+        });
+        match status {
+            Ok(_) =>
+                written += FRAME_SIZE,
+            Err(pa::Error::OutputUnderflowed) =>
+                eprintln!("underflow: written = {}", written),
+            _ => {status?;},
+        }
     }
 
     stream.stop()?;
--- src/nbsquare.rs
+++ src/nbsquare.rs
@@ -2,11 +2,11 @@ extern crate portaudio;
 
 use portaudio as pa;
 
-const SAMPLE_RATE: i32 = 48_000;
+const SAMPLE_RATE: i32 = 44_100;
 const FREQ: usize = 400;
 const MSECS: i32 = 3000;
 
-const FRAME_SIZE: u32 = SAMPLE_RATE as u32 / 128;
+const FRAME_SIZE: u32 = 1024;
 const SAMPLES: usize = SAMPLE_RATE as usize * MSECS as usize / 1000;
 const FRAMES: usize =  SAMPLES / FRAME_SIZE as usize;
 const SAMPLES_PER_HALFCYCLE: i32 = (SAMPLE_RATE / (2 * FREQ as i32)) as i32;
@@ -17,8 +17,8 @@ fn main() -> Result<(), pa::Error> {
             SAMPLE_RATE, MSECS, FREQ);
     println!("frame_size: {}, frames: {}, halfcycle: {}",
             FRAME_SIZE, FRAMES, SAMPLES_PER_HALFCYCLE);
-    println!("excess samples: {}",
-            SAMPLES as i32 - FRAME_SIZE as i32 * FRAMES as i32);
+    println!("last frame nominal size: {}",
+             FRAME_SIZE as i32 * (FRAMES as i32 + 1) - SAMPLES as i32);
 
     let pa = pa::PortAudio::new()?;
     let settings =
@@ -27,7 +27,8 @@ fn main() -> Result<(), pa::Error> {
     let mut cycle = 0;
     let mut sign = 0.8;
 
-    let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, ..}| {
+    let callback = move |pa::OutputStreamCallbackArgs {buffer, frames, ..}| {
+        assert_eq!(frames as u32, FRAME_SIZE);
         for i in 0..frames {
             buffer[i] = sign;
             cycle += 1;
@@ -35,7 +36,6 @@ fn main() -> Result<(), pa::Error> {
                 sign = -sign;
                 cycle = 0;
             }
-            
         }
         pa::Continue
     };
