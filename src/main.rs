50307078f6e8120f41249f84829e462cba881929
Bart Massey <bart@cs.pdx.edu>
2018-10-31 10:43:04 -0700

    moved to blocking output; not yet working

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::f64::consts::PI;
 
 const SAMPLE_RATE: u32 = 44_100;
 const FREQ: u32 = 1000;
-const MSECS: i32 = 3000;
+const MSECS: u32 = 3000;
 const FRAME_SIZE: u32 = 128;
 const NTABLE: usize = (SAMPLE_RATE * FREQ) as usize;
 
@@ -22,22 +22,28 @@ fn main() -> Result<(), pa::Error> {
         table[i] = (i as f32 * scale).sin();
     }
 
-    let mut sample = 0;
-
-    let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, ..}| {
-        for i in 0..frames {
-            buffer[i] = table[sample];
-            sample += 1;
-            if sample >= NTABLE {
-                sample = 0;
-            }
-        }
-        pa::Continue
-    };
-    
-    let mut stream = pa.open_non_blocking_stream(settings, callback)?;
+    let nsamples = MSECS * SAMPLE_RATE;
+    let mut stream = pa.open_blocking_stream(settings)?;
     stream.start()?;
-    pa.sleep(MSECS);
+    
+    let mut sample = 0;
+    let mut written = 0;
+    while written < nsamples {
+        let to_write = u32::min(nsamples - written, 1024);
+        println!("sample: {}, written: {}, to_write: {}", sample, written, to_write);
+        stream.write(to_write, |buffer| {
+            println!("writing");
+            for j in 0..(to_write as usize) {
+                buffer[j] = table[sample];
+                sample += 1;
+                if sample >= NTABLE {
+                    sample = 0;
+                }
+            }
+        })?;
+        written += to_write;
+    }
+
     stream.stop()?;
     stream.close()?;
     Ok(())
